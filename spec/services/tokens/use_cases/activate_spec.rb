4edf345475c9271c761d15d48b7516edf75274a9
Pedro Basilio <artiumdominus@protonmail.com>
2022-05-01 13:49:56 -0300

    add specs for use cases classes

--- spec/services/tokens/use_cases/activate_spec.rb
+++ spec/services/tokens/use_cases/activate_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Tokens::UseCases::Activate do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "::[]" do
+    let(:code) { token.id }
+    let(:result) { described_class[code:] }
+
+    context "when happy path" do
+      let(:token) { create :token }
+
+      it { expect(result).to match({ ok: { token: Token } }) }
+      it { expect(result.dig(:ok, :token).active).to be true }
+
+      it "validates the token code" do
+        allow(Tokens::Activate::Contract)
+          .to receive(:[]).and_return({ ok: { token: } })
+
+        expect(Tokens::Activate::Contract).to receive(:[]).once
+
+        result
+      end
+
+      it "activates the token" do
+        allow(Tokens::Activate)
+          .to receive(:[]).and_return({ ok: { token: } })
+        
+        expect(Tokens::Activate).to receive(:[]).once
+
+        result
+      end
+
+      it "deactivates previous tokens" do
+        allow(Tokens::DeactivatePrevious)
+          .to receive(:[]).and_return({ ok: { token: } })
+        
+        expect(Tokens::DeactivatePrevious).to receive(:[]).once
+
+        result
+      end
+    end
+  end
 end
--- spec/services/tokens/use_cases/generate_spec.rb
+++ spec/services/tokens/use_cases/generate_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Tokens::UseCases::Generate do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "::[]" do
+    let(:result) { described_class[email:] }
+
+    context "when happy path" do
+      let(:email) { "user@example.com" }
+      let(:user) { create :user, email: }
+      let(:token) { create :token, user: }
+
+      it { expect(result).to match({ ok: { success: true } }) }
+
+      it "validates the email" do
+        allow(Tokens::Generate::Contract)
+          .to receive(:[]).and_return({ ok: { email: } })
+
+        expect(Tokens::Generate::Contract).to receive(:[]).once
+
+        result
+      end
+
+      it "find or creates an user" do
+        allow(Users::FindOrCreate)
+          .to receive(:[]).and_return({ ok: { user: } })
+
+        expect(Users::FindOrCreate).to receive(:[]).once
+
+        result
+      end
+
+      it "creates a token" do
+        allow(Tokens::Create)
+          .to receive(:[]).and_return({ ok: { token: } })
+        
+        expect(Tokens::Create).to receive(:[]).once
+
+        result
+      end
+
+      it "sends token activation email" do
+        allow(Tokens::PublishActivationEmail)
+          .to receive(:[]).and_return({ ok: { success: true } })
+
+        expect(Tokens::PublishActivationEmail).to receive(:[]).once
+
+        result
+      end
+    end
+  end
 end
